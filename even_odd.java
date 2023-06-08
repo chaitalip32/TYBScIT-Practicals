e26425bd20f74d1baed41fda1f96343fe2e527fb
Chaitali <chaitalip585@gmail.com>
2023-06-08 23:04:28 +0530

    Giving Initial commit

---
+++ even_odd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class even_odd
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number:");
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println(n+" is even");
+}
+else
+{
+System.out.println(n+" is odd");
+}
+}
+}
